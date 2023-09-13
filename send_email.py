6cbe6f82d4440311a35452af58ba551b2c4ea9be
unknown <dakedak670@gmail.com>
2023-09-13 11:46:55 +0600

    added addresses to send message

--- send_email.py
+++ send_email.py
@@ -1,3 +1,7 @@
 def send_mail(email_text, address, title):
     message = email_text.format()
     return message + address + title
+
+def addresses(filepath):
+    with open (filepath, 'r') as file:
+        return file.read()
